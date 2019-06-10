96b574afc1728b93522a7d805805a5bf3c4e231f
LinskRuis.3.6GHz <mgr9525@qq.com>
2019-06-10 18:50:52 +0800

    json conf

---
+++ JsonConfig.go
@@ -0,0 +1,71 @@
+package ruisUtil
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"linskruis/go-ruisutil/ruisio"
+)
+
+type JsonConfig struct {
+	path string
+	root map[string]interface{}
+}
+
+func NewJsonConfig(path string) *JsonConfig {
+	var e = new(JsonConfig)
+	e.path = path
+	e.root = make(map[string]interface{})
+	e.init()
+	return e
+}
+
+func (e *JsonConfig) init() {
+	defer Recovers("JsonConfig.init")
+	if e.path != "" && ruisio.PathExists(e.path) {
+		conts, err := ioutil.ReadFile(e.path)
+		if err == nil {
+			fmt.Println("readTo:" + e.path + "! conts:" + string(conts))
+			err := json.Unmarshal(conts, &e.root)
+			if err != nil {
+				fmt.Println("readTo:err")
+				e.root = map[string]interface{}{}
+			}
+		}
+	}
+	fmt.Println("init:ok")
+}
+
+func (e *JsonConfig) Reinit(bts []byte) {
+	defer Recovers("JsonConfig.reinit")
+	json.Unmarshal(bts, &e.root)
+}
+
+func (e *JsonConfig) Gets(key string) string {
+	ret, ext := e.root[key]
+	if ext {
+		return ret.(string)
+	}
+	return ""
+}
+func (e *JsonConfig) Sets(key string, value string) {
+	e.root[key] = value
+	go e.Save()
+}
+
+func (e *JsonConfig) Save() {
+	defer Recovers("JsonConfig.save")
+
+	if e.path != "" {
+		js, err := json.Marshal(e.root)
+		if err == nil {
+			//fmt.Println("saveTo:"+ruisApp.dataPath+e.name+"! js:"+string(js))
+			err = ioutil.WriteFile(e.path, js, 0644)
+			if err != nil {
+				fmt.Println("conf save err")
+			}
+		} else {
+			fmt.Println("conf save err")
+		}
+	}
+}
