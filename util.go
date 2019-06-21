d5aeef2e3daeebca295269649555072055cef5b5
LinskRuis.3.6GHz <mgr9525@qq.com>
2019-06-21 09:18:11 +0800

    bugs

--- util.go
+++ util.go
@@ -1,6 +1,10 @@
 package ruisUtil
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
 
 func Recovers(name string) {
 	if err := recover(); err != nil {
@@ -21,4 +25,143 @@ func Short2Bytes(v int) []byte {
 	bts[0] = byte((v >> 8) & 0xff)
 	bts[1] = byte(v & 0xff)
 	return bts
+}
+
+func Int2Bytes(v int64, len int) []byte {
+	buf := &bytes.Buffer{}
+	switch len {
+	case 8:
+		var tmp = int8(v)
+		err := binary.Write(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Write err")
+		}
+		break
+	case 16:
+		var tmp = int16(v)
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Write err")
+		}
+		break
+	case 32:
+		var tmp = int32(v)
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Write err")
+		}
+		break
+	case 64:
+		var tmp = int64(v)
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Write err")
+		}
+		break
+	}
+	return buf.Bytes()
+}
+func Bytes2Int(v []byte, len int) int64 {
+	buf := bytes.NewBuffer(v)
+	switch len {
+	case 8:
+		var tmp int8
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Read err")
+		}
+		return int64(tmp)
+	case 16:
+		var tmp int16
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Read err")
+		}
+		return int64(tmp)
+	case 32:
+		var tmp int32
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Read err")
+		}
+		return int64(tmp)
+	case 64:
+		var tmp int64
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Read err")
+		}
+		return int64(tmp)
+	}
+
+	return 0
+}
+func UInt2Bytes(v int64, len int) []byte {
+	buf := &bytes.Buffer{}
+	switch len {
+	case 8:
+		var tmp = uint8(v)
+		err := binary.Write(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Write err")
+		}
+		break
+	case 16:
+		var tmp = uint16(v)
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Write err")
+		}
+		break
+	case 32:
+		var tmp = uint32(v)
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Write err")
+		}
+		break
+	case 64:
+		var tmp = uint64(v)
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Write err")
+		}
+		break
+	}
+	return buf.Bytes()
+}
+func Bytes2UInt(v []byte, len int) uint64 {
+	buf := bytes.NewBuffer(v)
+	switch len {
+	case 8:
+		var tmp uint8
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Read err")
+		}
+		return uint64(tmp)
+	case 16:
+		var tmp uint16
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Read err")
+		}
+		return uint64(tmp)
+	case 32:
+		var tmp uint32
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Read err")
+		}
+		return uint64(tmp)
+	case 64:
+		var tmp uint64
+		err := binary.Read(buf, binary.BigEndian, &tmp)
+		if err != nil {
+			panic("binary.Read err")
+		}
+		return uint64(tmp)
+	}
+
+	return 0
 }
