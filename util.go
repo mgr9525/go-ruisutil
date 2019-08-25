98a1025e091ae54a39dfdddd1c6aa0610abf5317
LinskRuis.3.6GHz <mgr9525@qq.com>
2019-08-25 22:09:33 +0800

    defers+

--- util.go
+++ util.go
@@ -3,6 +3,7 @@ package ruisUtil
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
 )
 
@@ -13,7 +14,16 @@ func Recovers(name string, handle ErrHandle) {
 		fmt.Print("ruisRecover(" + name + "):")
 		fmt.Println(err) // 这里的err其实就是panic传入的内容，55
 		if handle != nil {
-			handle(err)
+			var rterr error
+			switch err.(type) {
+			case error:
+				rterr = err.(error)
+				break
+			default:
+				rterr = errors.New("not found what error!")
+				break
+			}
+			handle(rterr)
 		}
 	}
 }
