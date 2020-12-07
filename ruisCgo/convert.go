c090bcfc80e50fe895982e9481a5af47692d1294
ryanmao <ryanmao@tencent.com>
2020-12-07 11:32:57 +0800

    convert struct bytes

---
+++ ruisCgo/convert.go
@@ -0,0 +1,55 @@
+package ruisCgo
+
+// #include<string.h>
+import "C"
+import (
+	"errors"
+	"reflect"
+	"unsafe"
+)
+
+type SliceMock struct {
+	addr unsafe.Pointer
+	len  int
+	cap  int
+}
+
+func Struct2Bytes(pt interface{}) ([]byte, error) {
+	if pt == nil {
+		return nil, errors.New("param is nil")
+	}
+	vf := reflect.ValueOf(pt)
+	if vf.IsNil() {
+		return nil, errors.New("param is err")
+	}
+	data := pt
+	if vf.Type().Kind() == reflect.Ptr {
+		data = vf.Elem().Interface()
+	}
+	ln := unsafe.Sizeof(data)
+	mock := &SliceMock{
+		addr: unsafe.Pointer(&data),
+		cap:  int(ln),
+		len:  int(ln),
+	}
+	bts := *(*[]byte)(unsafe.Pointer(mock))
+	rtbts := make([]byte, mock.len)
+	copy(rtbts, bts)
+	return rtbts, nil
+}
+func Bytes2Struct(pt interface{}, bts []byte) error {
+	if pt == nil {
+		return errors.New("param is nil")
+	}
+	vf := reflect.ValueOf(pt)
+	if vf.Type().Kind() != reflect.Ptr || vf.IsNil() {
+		return errors.New("param is err")
+	}
+	data := vf.Elem().Interface()
+	ln := unsafe.Sizeof(data)
+	dataptr := uintptr(unsafe.Pointer(&data))
+	sct := uintptr(unsafe.Pointer(&bts[0]))
+	C.memcpy(unsafe.Pointer(dataptr), unsafe.Pointer(sct), C.ulong(ln))
+	vf.Elem().Set(reflect.ValueOf(data))
+	return nil
+}
