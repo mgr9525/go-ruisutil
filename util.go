0c4583b203214e27f113d025edb889a1dd4b5710
ryanmao <ryanmao@tencent.com>
2020-12-09 16:03:55 +0800

    byte int convert+float

--- util.go
+++ util.go
@@ -10,6 +10,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math"
 	"math/rand"
 	"os"
 	"sync/atomic"
@@ -56,6 +57,42 @@ func LitIntToByte(data int64, ln int) []byte {
 		rt[i] = byte(data >> (i * 8))
 	}
 	return rt
+}
+
+// BigEndian
+func BigByteToFloat32(data []byte) float32 {
+	v := BigByteToInt(data)
+	return math.Float32frombits(uint32(v))
+}
+func BigFloatToByte32(data float32) []byte {
+	v := math.Float32bits(data)
+	return BigIntToByte(int64(v), 4)
+}
+func BigByteToFloat64(data []byte) float64 {
+	v := BigByteToInt(data)
+	return math.Float64frombits(uint64(v))
+}
+func BigFloatToByte64(data float64) []byte {
+	v := math.Float64bits(data)
+	return BigIntToByte(int64(v), 8)
+}
+
+// LittleEndian
+func LitByteToFloat32(data []byte) float32 {
+	v := LitByteToInt(data)
+	return math.Float32frombits(uint32(v))
+}
+func LitFloatToByte32(data float32) []byte {
+	v := math.Float32bits(data)
+	return LitIntToByte(int64(v), 4)
+}
+func LitByteToFloat64(data []byte) float64 {
+	v := LitByteToInt(data)
+	return math.Float64frombits(uint64(v))
+}
+func LitFloatToByte64(data float64) []byte {
+	v := math.Float64bits(data)
+	return LitIntToByte(int64(v), 8)
 }
 
 func StructInterturn(src, dist interface{}) error {
