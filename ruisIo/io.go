8e05224f45bcac9a51709a8b84c44f534c4eba02
LinsRuis.HW1 <mgr9525@qq.com>
2024-06-16 23:18:15 +0800

    io rea

--- ruisIo/io.go
+++ ruisIo/io.go
@@ -3,10 +3,11 @@ package ruisIo
 import (
 	"context"
 	"errors"
+	"fmt"
 	"io"
 )
 
-func TcpRead(ctx context.Context, conn io.Reader, ln uint) ([]byte, error) {
+/*func TcpRead(ctx context.Context, conn io.Reader, ln uint) ([]byte, error) {
 	if conn == nil || ln <= 0 {
 		return nil, errors.New("handleRead ln<0")
 	}
@@ -31,8 +32,9 @@ func TcpRead(ctx context.Context, conn io.Reader, ln uint) ([]byte, error) {
 		}
 	}
 	return bts, nil
-}
-func TcpWrite(ctx context.Context, conn io.Writer, bts []byte) error {
+}*/
+
+/*func TcpWrite(ctx context.Context, conn io.Writer, bts []byte) error {
 	ln := len(bts)
 	if conn == nil || ln <= 0 {
 		return errors.New("handleRead ln<0")
@@ -44,10 +46,66 @@ func TcpWrite(ctx context.Context, conn io.Writer, bts []byte) error {
 			return errors.New("context dead")
 		}
 		n, err := conn.Write(bts[wn:])
+		wn += n
 		if err != nil {
 			return err
 		}
-		wn += n
+		if n <= 0 && wn < ln {
+			return errors.New("conn abort")
+		}
+	}
+	return nil
+}*/
+
+func IoReadAll(ctx context.Context, rdr io.Reader, ln uint) ([]byte, error) {
+	rn := uint(0)
+	bts := make([]byte, ln)
+	for {
+		if EndContext(ctx) {
+			return nil, errors.New("ctx end")
+		}
+		n, err := rdr.Read(bts[rn:])
+		if n > 0 {
+			rn += uint(n)
+		}
+		if rn >= ln {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		if n <= 0 {
+			return nil, fmt.Errorf("read ln=0 broker")
+		}
+	}
+	if rn != ln {
+		return nil, fmt.Errorf("read len err:%d/%d", rn, ln)
+	}
+	return bts, nil
+}
+func IoWriteAll(ctx context.Context, wtr io.Writer, bts []byte) error {
+	wn := 0
+	ln := len(bts)
+	for {
+		if EndContext(ctx) {
+			return errors.New("ctx end")
+		}
+		n, err := wtr.Write(bts[wn:])
+		if n > 0 {
+			wn += n
+		}
+		if wn >= ln {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if n <= 0 {
+			return fmt.Errorf("write ln=0 broker")
+		}
+	}
+	if wn != ln {
+		return fmt.Errorf("write len err:%d/%d", wn, ln)
 	}
 	return nil
 }
